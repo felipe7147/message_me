b9d73bff06e9101a7108a8843b945c65479bd0b0
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2022-02-28 12:29:40 -0300

    Modify and broadcast messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,11 @@
 class MessagesController < ApplicationController
   before_action :require_user
 
-  def create 
+  def create
     message = current_user.messages.build(message_params)
-    if message.save 
+    if message.save
+      ActionCable.server.broadcast("chatroom_channel",
+                                  { foo: message.body})
       redirect_to root_path
     end
   end
